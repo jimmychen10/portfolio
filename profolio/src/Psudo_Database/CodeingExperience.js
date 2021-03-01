ddf9cabbabe8de7cb4149ff40ea428a92fdbb513
Jimmy Chen <jimmychen1996@yahoo.com>
2021-03-01 00:39:09 -0800

    added more languages

--- profolio/src/Psudo_Database/CodeingExperience.js
+++ profolio/src/Psudo_Database/CodeingExperience.js
@@ -9,69 +9,85 @@ import JavaIcon from "../Images/Language Icons/java.png"
 import JavaScriptIcon from "../Images/Language Icons/JavaScript-logo.png"
 import PythonIcon from "../Images/Language Icons/python-logo.png"
 import ReactIcon from "../Images/Language Icons/React-logo.png"
+import SqlIcon from "../Images/Language Icons/sql-icon.png"
+import MysqlIcon from "../Images/Language Icons/mysql-icon.png"
+
 // import t from "../Images/Language Icons"
 const CodingExperience =[
 
     {
         id:1,
-        name: "Bootstrap",
-        y:1,
-        image: BootstrapIcon
-    },
-    {
-        id:2,
-        name: "GitBash",
-        y:1,
-        image: GitBashIcon
-    },
-    {
-        id:3,
-        name: "git",
-        y:1,
-        image:GitIcon
-    },
-    {
-        id:3,
-        name: "github",
-        y:1,
-        image:GithubIcon
-    },
-    {
-        id:4,
-        name: "Javascript",
-        y:1,
-        image: JavaScriptIcon
-    },
-    {
-        id:5,
         name: "React",
         y:1,
         image: ReactIcon
     },
     {
-        id:6,
+        id:2,
+        name: "Javascript",
+        y:1,
+        image: JavaScriptIcon
+    },
+    {
+        id:3,
         name: "HTML",
         y:2,
         image: HtmlIcon
     },
     {
-        id:7,
+        id:4,
         name: "CSS",
         y:2,
         image: CSSIcon
     },
     {
-        id:8,
+        id:5,
         name: "Python",
         y:3,
         image: PythonIcon
     },
     {
-        id:9,
+        id:6,
         name: "Java",
         y:3,
         image: JavaIcon
     },
+    {
+        id:7,
+        name: "SQL",
+        y:1,
+        image: SqlIcon
+    },
+    {
+        id:8,
+        name: "MySQL",
+        y:1,
+        image: MysqlIcon
+    },
+    {
+        id:9,
+        name: "Bootstrap",
+        y:1,
+        image: BootstrapIcon
+    },
+    {
+        id:10,
+        name: "GitBash",
+        y:1,
+        image: GitBashIcon
+    },
+    {
+        id:11,
+        name: "git",
+        y:1,
+        image:GitIcon
+    },
+    {
+        id:12,
+        name: "github",
+        y:1,
+        image:GithubIcon
+    },
+
 ]
 
 export default CodingExperience
